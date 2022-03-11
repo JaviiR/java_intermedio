2b91202f8dc51479d530aea6a217a3e7fe665720
JaviiR <alexircaa@gmail.com>
2022-03-10 19:06:25 -0500

    Fin del fichero Recursividad

---
+++ src/Recursividad/Clase_Principal.java
@@ -0,0 +1,9 @@
+package Recursividad;
+
+public class Clase_Principal {
+    public static void main(String[] args) {
+        Recursividad recursividad=new Recursividad();
+        recursividad.Imprimir(1);
+    }
+    
+}
